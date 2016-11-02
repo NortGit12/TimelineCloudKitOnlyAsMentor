872a35a0cf14eeda93ac2981a1cff74183b41049
Jeff Norton <jeff12ntn@gmail.com>
2016-11-02 16:17:01 -0600

    Reduce code repetition done

--- TimelineCloudKitOnlyAsMentor/AddPostTableViewController.swift
+++ TimelineCloudKitOnlyAsMentor/AddPostTableViewController.swift
@@ -8,78 +8,15 @@
 
 import UIKit
 
-class AddPostTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class AddPostTableViewController: UITableViewController, PhotoSelectViewControllerDelegate {
     
     //==================================================
-    // MARK: - Properties
+    // MARK: - _Properties
     //==================================================
     
-    @IBOutlet weak var postImageView: UIImageView!
-    @IBOutlet weak var selectImageButton: UIButton!
     @IBOutlet weak var captionTextField: UITextField!
-    
-    //==================================================
-    // MARK: - General
-    //==================================================
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-    
-    //==================================================
-    // MARK: - Table view data source
-    //==================================================
-
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    var image: UIImage?
+    var photoSelectViewController: PhotoSelectViewController?
     
     //==================================================
     // MARK: - Actions
@@ -87,7 +24,7 @@ class AddPostTableViewController: UITableViewController, UIImagePickerController
     
     @IBAction func addPostButtonTapped(_ sender: UIButton) {
         
-        guard let image = postImageView.image
+        guard let image = self.image
             , let caption = captionTextField.text
             , caption.characters.count > 0 else {
                 
@@ -113,54 +50,27 @@ class AddPostTableViewController: UITableViewController, UIImagePickerController
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func selectImageButtonTapped(_ sender: UIButton) {
+    //==================================================
+    // MARK: - Navigation
+    //==================================================
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        let imagePickerController = UIImagePickerController()
-        imagePickerController.delegate = self
-        
-        let photoAlertController = UIAlertController(title: "Select Image Source", message: nil, preferredStyle: .actionSheet)
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        photoAlertController.addAction(cancelAction)
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+        if segue.identifier == "toPhotoSelectEmbedSegue" {
             
-            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
-                
-                imagePickerController.sourceType = .camera
-                self.present(imagePickerController, animated: true, completion: nil)
-            }
-            
-            photoAlertController.addAction(cameraAction)
+            let embeddedViewController = segue.destination as? PhotoSelectViewController
+            embeddedViewController?.delegate = self
         }
-        
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            
-            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
-                
-                imagePickerController.sourceType = .photoLibrary
-                self.present(imagePickerController, animated: true, completion: nil)
-            }
-            
-            photoAlertController.addAction(photoLibraryAction)
-        }
-        
-        present(photoAlertController, animated: true, completion: nil)
     }
     
     //==================================================
-    // MARK: - UIImagePickerControllerDelegate
+    // MARK: - PhotoSelectViewControllerDelegate
     //==================================================
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func selected(image: UIImage) {
         
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            
-            selectImageButton.setTitle("", for: .normal)
-            postImageView.image = image
-        }
-        
-        dismiss(animated: true, completion: nil)
+        self.image = image
     }
 }
 
---
+++ TimelineCloudKitOnlyAsMentor/PhotoSelectViewController.swift
@@ -0,0 +1,90 @@
+//
+//  PhotoSelectViewController.swift
+//  TimelineCloudKitOnlyAsMentor
+//
+//  Created by Jeff Norton on 11/2/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+import UIKit
+
+protocol PhotoSelectViewControllerDelegate: class {
+    
+    func selected(image: UIImage)
+}
+
+class PhotoSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //==================================================
+    // MARK: - Properties
+    //==================================================
+    
+    weak var delegate: PhotoSelectViewControllerDelegate?
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var selectImageButton: UIButton!
+    
+    //==================================================
+    // MARK: - General
+    //==================================================
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //==================================================
+    // MARK: - Actions
+    //==================================================
+    
+    @IBAction func selectImageButtonTapped(_ sender: UIButton) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let photoAlertController = UIAlertController(title: "Select Image Source", message: nil, preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        photoAlertController.addAction(cancelAction)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
+                
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }
+            
+            photoAlertController.addAction(cameraAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
+                
+                imagePickerController.sourceType = .photoLibrary
+                self.present(imagePickerController, animated: true, completion: nil)
+            }
+            
+            photoAlertController.addAction(photoLibraryAction)
+        }
+        
+        present(photoAlertController, animated: true, completion: nil)
+    }
+    
+    //==================================================
+    // MARK: - UIImagePickerControllerDelegate
+    //==================================================
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            delegate?.selected(image: image)
+            selectImageButton.setTitle("", for: .normal)
+            postImageView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
